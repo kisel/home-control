63338d2724eed50b100108213936a57b29d22d1a
Anton Kiselev <anton.kisel@gmail.com>
2021-07-27 14:42:37 +0700

    optional file output via env.OUTPUT

--- src/config.ts
+++ src/config.ts
@@ -4,3 +4,7 @@ export const nodeid = process.env.NODEID || 'NODE-D5C683'
 export const ctrltopic = `switch/${nodeid}/ctrl`
 export const mqtt_host = process.env.MQTT_HOST
 export const import_file = process.env.IMPORT_FILE
+export const output_dir = process.env.OUTPUT
+export const lastwill_timeout = 5000
+
+
--- src/mqtt.ts
+++ src/mqtt.ts
@@ -1,7 +1,7 @@
 const mqtt = require('mqtt')
 const fs = require('fs')
 const path = require('path')
-import {nodeid, ctrltopic, mqtt_host} from "./config"
+import {nodeid, ctrltopic, mqtt_host, lastwill_timeout, output_dir} from "./config"
 
 export function start_mqtt_server(onEvent) {
     const client  = mqtt.connect(`mqtt://${mqtt_host}:1883/`)
@@ -9,21 +9,22 @@ export function start_mqtt_server(onEvent) {
     client.on('connect', function () {
       client.subscribe('switch/#', function (err) {
         if (!err) {
-          client.publish('presence', 'Hello mqtt')
+          client.publish('presence', 'Hello mqtt from robohome')
         }
       })
     })
 
     let handlers = {}
-    const evtlog = fs.createWriteStream('evtlog.jsonl', {flags: 'a'})
-    const uptimelog = fs.createWriteStream('uptime.jsonl', {flags: 'a'})
-    const timeout = 5000
+    let evtlog = null
+    let uptimelog = null
+    if (output_dir) {
+        evtlog = fs.createWriteStream(path.join(output_dir, 'evtlog.jsonl'), {flags: 'a'})
+        uptimelog = fs.createWriteStream(path.join('uptime.jsonl'), {flags: 'a'})
+    }
 
     client.on('message', function (topic, message) {
       const ma = topic.match(/^switch[\/](NODE-[^\/]{6})[\/]status/)
       const msg = message.toString().match(/^uptime=(\d+) rssi=([-\d]+) ch0=(\d+)/)
-      console.log(ma)
-      console.log(msg)
       if (ma && msg) {
           const payload = {
               ts: new Date(),
@@ -32,8 +33,10 @@ export function start_mqtt_server(onEvent) {
               rssi: msg[2],
               ch0: msg[3],
           }
-          console.log(payload)
-          evtlog.write(JSON.stringify(payload)+'\n')
+          //console.log(`${topic} ${message}`)
+          if (evtlog) {
+              evtlog.write(JSON.stringify(payload)+'\n')
+          }
         
           handlers[payload.node] = handlers[payload.node] || {}
 
@@ -44,14 +47,13 @@ export function start_mqtt_server(onEvent) {
 
           handlers[payload.node].tmr = setTimeout(()=>{
               console.log(`Got event: `, payload)
-              uptimelog.write(JSON.stringify(payload)+'\n')
+              if (uptimelog) {
+                  uptimelog.write(JSON.stringify(payload)+'\n')
+              }
               onEvent(payload)
-          }, timeout)
+          }, lastwill_timeout)
 
       }
-      // message is Buffer
-      console.log(message.toString())
-      //client.end()
     })
     return client;
 }
