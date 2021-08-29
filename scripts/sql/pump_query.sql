b3ed4d3796d9c8dfeecb59ebb23aa97bdf10529c
Anton Kiselev <anton.kisel@gmail.com>
2021-08-30 03:43:37 +0700

    add pump_query.sql gantt query with colors & classes

--- scripts/sql/pump_query.sql
+++ scripts/sql/pump_query.sql
@@ -41,4 +41,46 @@ WHERE
 GROUP BY 1,2
 ORDER BY 1,2
 
+-- gantt chart lead end time
+-- Requires class, 'color mapping' for Grafana chart
+SELECT
+  node as name,
+  ts-make_interval(secs => uptime) AS time,
+  ts as end_time,
+  CASE
+    WHEN uptime < 60 then 'grey'
+    WHEN uptime < 600 then 'green'
+    WHEN uptime < 1200 then 'yellow'
+    ELSE 'orange'
+  END as color,
+  CASE
+    WHEN uptime < 60 then '< 60sec'
+    WHEN uptime < 600 then '1-10min'
+    WHEN uptime < 1200 then '10-20min'
+    ELSE '20min+'
+  END as class
+
+FROM waterpump_journals
+WHERE
+  $__timeFilter("ts")
+
+UNION ALL
+
+SELECT
+  name,
+  time,
+  end_time,
+  'red' as color,
+  'cutoff' as class
+FROM (
+	SELECT
+	node as name,
+	ts AS time,
+	ch0,
+	lead(ts) over (order by ts) as end_time
+	FROM waterpump_logs
+    WHERE $__timeFilter("ts")
+) with_end
+WHERE ch0 = '1'
+ORDER BY time
 
