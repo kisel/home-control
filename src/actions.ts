857cda786b24c2ba0444a53ed7b441c0e494e92c
Anton Kiselev <anton.kisel@gmail.com>
2021-09-05 22:53:27 +0700

    auto-generate missing action IDs

--- src/actions.ts
+++ src/actions.ts
@@ -1,8 +1,17 @@
 import * as fse from 'fs-extra'
 import * as YAML from 'yaml';
 import { ActionsFile } from './models';
+import * as crypto from 'crypto'
 
 export async function readActionsFile(filename: string): Promise<ActionsFile> {
-    const data = await fse.readFile(filename, 'utf8');
-    return YAML.parse(data);
+    const data = await fse.readFile(filename, 'utf8').then(d => YAML.parse(d) as ActionsFile);
+    const actions = data.actions.map((a) => {
+        if (a.id) {
+            return a;
+        } else {
+            return {...a, id: crypto.createHash('sha1').update(JSON.stringify(a)).digest('hex') }
+        }
+    })
+    return {actions};
 }
+
